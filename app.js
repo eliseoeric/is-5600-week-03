323807a8e17df603b2e077676b94382dd1523153
samudalasuresh <darshini.amudala@gmail.com>
2024-11-11 00:16:43 +0000

    Completed the task as per the lab instructions

---
+++ app.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const express = require('express');
+const EventEmitter = require('events');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const chatEmitter = new EventEmitter();
+
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+function respondJson(req, res) {
+  res.json({
+    text: 'hi',
+    numbers: [1, 2, 3],
+  });
+}
+
+function respondNotFound(req, res) {
+  res.status(404).send('Not Found');
+}
+
+function respondEcho(req, res) {
+  const { input = '' } = req.query;
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    charCount: input.length,
+    backwards: input.split('').reverse().join(''),
+  });
+}
+
+function chatApp(req, res) {
+  res.sendFile(path.join(__dirname, '/chat.html'));
+}
+
+function respondChat(req, res) {
+  const { message } = req.query;
+  chatEmitter.emit('message', message);
+  res.end();
+}
+
+function respondSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+  });
+  const onMessage = (message) => res.write(`data: ${message}\n\n`);
+  chatEmitter.on('message', onMessage);
+  res.on('close', () => chatEmitter.off('message', onMessage));
+}
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', chatApp);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/chat', respondChat);
+app.get('/sse', respondSSE);
+app.get('*', respondNotFound);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
