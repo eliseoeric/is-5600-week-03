25277dfc912bea65bc12f1c51bee3800672d65a8
Arya003-slu <aryavardhanreddy.challa@slu.edu>
2024-11-13 04:48:16 +0000

    Pending changes exported from your codespace

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const url = require('url');
+
+const port = process.env.PORT || 3000;
+
+// note that typically the variables here are `req` and `res` but we are using `request` and `response` for clarity
+const server = http.createServer(function(request, response) {
+  response.end("hi");
+});
+
+server.listen(port, function() {
+  console.log(`Server is listening on port ${port}`);
+});
+// modify the server code to look like this
+const server = http.createServer(function(request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ text: 'hi', number: [1, 2, 3] }));
+  });
